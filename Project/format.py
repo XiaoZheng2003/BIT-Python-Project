3c7f207fd7f55d893c602650c532f7600ef2bfb9
XiaoZheng2003 <1345486584@qq.com>
2022-08-28 16:16:41 +0800

    更新代码

--- Project/format.py
+++ Project/format.py
@@ -6,7 +6,9 @@ for j in range(33):
         ls=f[i].readline().replace('\n','').split(',')
         if j==0:
             continue
-        fw=open("result/city/{}_{}.csv".format(ls[0],ls[1]),"a",encoding="gbk")
         if i==0:
+            fw=open("result/city/{}_{}.csv".format(ls[0],ls[1]),"w",encoding="gbk")
             fw.write("年份,平均高温,平均低温,极端高温,极端低温\n")
+        else:
+            fw=open("result/city/{}_{}.csv".format(ls[0],ls[1]),"a",encoding="gbk")
         fw.write("20{},{},{},{},{}\n".format(i+11,ls[2],ls[3],ls[4],ls[5]))
